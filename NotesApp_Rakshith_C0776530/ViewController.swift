16005540f07e67be2203041dcab58bbb6fd9cdc7
Rakshith <rakshithbu92@gmail.com>
2020-06-25 16:46:11 -0700

    Initial Commit

---
+++ NotesApp_Rakshith_C0776530/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp_Rakshith_C0776530
+//
+//  Created by user176082 on 6/25/20.
+//  Copyright © 2020 rakshith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
